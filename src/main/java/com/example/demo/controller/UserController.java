f5a62b849927145e57cc195635b99905dc697c0e
Horacio <horacio.valenzueladavinci.edu.ar>
2024-06-03 22:38:07 -0300

    Add changes

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,30 +4,34 @@ package com.example.demo.controller;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/users/")
-    public List<User> findAll(){
-        return userService.findAll();
-    }
-
-
-    @GetMapping("/users/{id}")
+    @GetMapping("/{id}")
     public Optional<User> searchUserById(@PathVariable Long id){
         return userService.findById(id);
     }
 
+    @PostMapping("/")
+    public User create(@RequestBody Map<String, Object> requestBody) {
+        String name = requestBody.get("name").toString();
+        String surname = requestBody.get("surname").toString();
+        User user = new User(name, surname);
+        return userService.createUser(user);
+    }
 
-
+    @GetMapping("/")
+    public List<User> findAll(){
+        return userService.findAll();
+    }
 
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -14,8 +14,9 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public void createUser(User user){
+    public User createUser(User user){
         userRepository.save(user);
+        return user;
     }
 
     public Optional<User> findById(Long id) {

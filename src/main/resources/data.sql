912fe6a3c1976859f20c4e352d523b5d5b452f79
Horacio Valenzuela <horacio.valenzuela@davinci.edu.ar>
2024-06-12 22:57:18 -0300

    Add new data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -25,15 +25,31 @@ CREATE TABLE IF NOT EXISTS products (
     description VARCHAR(255)
 );
 
+CREATE TABLE IF NOT EXISTS order_products (
+    order_id BIGINT,
+    product_id BIGINT,
+    PRIMARY KEY (order_id, product_id),
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
+CREATE TABLE IF NOT EXISTS user_orders (
+    user_id BIGINT,
+    order_id BIGINT,
+    PRIMARY KEY (user_id, order_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (order_id) REFERENCES orders(id)
+);
+
 INSERT INTO users (name, surname, age) VALUES ('Juan', 'Pérez', 32);
 INSERT INTO users (name, surname, age) VALUES ('María', 'García', 25);
 INSERT INTO users (name, surname, age) VALUES ('Carlos', 'Rodríguez', 42);
 INSERT INTO users (name, surname, age) VALUES ('Ana', 'López', 34);
 
+/*
 INSERT INTO products (price, description) VALUES (500.00, 'Smartphone de última generación');
 INSERT INTO products (price, description) VALUES (100.00, 'Auriculares inalámbricos con cancelación de ruido');
 
 INSERT INTO orders (description, user_id) VALUES ('Compra de teléfono móvil y auriculares inalámbricos', 1);
 INSERT INTO orders (description, user_id) VALUES ('Compra de teléfono móvil', 2);
-
-
+*/
